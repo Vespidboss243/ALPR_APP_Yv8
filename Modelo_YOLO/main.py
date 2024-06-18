8cc0fb7064db6c287c182fe8c933d46d5fb7fe29
Vespidboss <77710376+Vespidboss243@users.noreply.github.com>
2024-06-18 16:29:34 -0500

    Agregacion  del Modelo

---
+++ Modelo_YOLO/main.py
@@ -0,0 +1,62 @@
+from ultralytics import YOLO
+import cv2
+from util import read_license_plate, send_license_plate
+
+results = {}
+placa_anterior = None
+
+license_plate_detector = YOLO('ModelitoPlacasBestOptimizado.pt')
+
+# Cargar Video
+cap = cv2.VideoCapture(0)
+
+
+
+# Procesar cada frame
+frame_nmr = -1
+ret = True
+while ret:
+    frame_nmr += 1
+    ret, frame = cap.read()
+    if ret:
+        results[frame_nmr] = {}
+        license_plates = license_plate_detector(frame)[0]
+        for license_plate in license_plates.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = license_plate
+
+            # Recorte de la imagen
+            license_plate_crop = frame[int(y1):int(y2), int(x1): int(x2), :]
+
+            # Transformación de la imagen
+            license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+            #_, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 255), 2)
+            #text = f"Plate Score: {int(score * 100)}%"
+            #cv2.putText(frame, text, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_DUPLEX, 0.9, (255, 255, 255), 2)
+            # Lectura de la placa
+            license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_gray)
+            print(license_plate_text)
+            cv2.imwrite('placa.jpg', license_plate_crop)
+            if license_plate_text is not None:
+                print(license_plate_text)
+                if (license_plate_text != placa_anterior):
+                    placa_anterior = license_plate_text
+                    resultado =  send_license_plate(license_plate_text)
+                    print(resultado)
+                # Bounding Box
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (43, 57, 192), 2)
+                text = f"{license_plate_text} Score: {int(license_plate_text_score * 100)}%"
+                cv2.putText(frame, text, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_DUPLEX, 0.9, (43, 57, 192), 1)
+               # Camara en vivo
+    cv2.imshow('Camara en vivo', frame)
+
+    # Leer el siguiente frame
+    ret, frame = cap.read()
+
+    # Salir si se presiona 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Liberar recursos
+cap.release()
+cv2.destroyAllWindows()
---
+++ Modelo_YOLO/util.py
@@ -0,0 +1,89 @@
+import string
+import easyocr
+import requests
+import json
+import pytesseract
+
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Initialize the OCR reader
+reader = easyocr.Reader(['es'], gpu=True)
+
+# Mapping dictionaries for character conversion
+dict_char_to_int = {'O': '0',
+                    'I': '1',
+                    'J': '3',
+                    'A': '4',
+                    'G': '6',
+                    'S': '5'}
+
+dict_int_to_char = {'0': 'O',
+                    '1': 'I',
+                    '3': 'J',
+                    '4': 'A',
+                    '6': 'G',
+                    '5': 'S'}
+
+
+def license_complies_format(text):
+    
+    if len(text) != 6:
+        return False
+
+    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in string.ascii_uppercase or text[2] in dict_int_to_char.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[4] in dict_char_to_int.keys()) and \
+       (text[5] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[5] in dict_char_to_int.keys()):
+       
+        return True
+    else:
+        return False
+
+
+def format_license(text):
+    
+    license_plate_ = ''
+    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 2: dict_int_to_char,
+               3: dict_char_to_int, 4: dict_char_to_int,5: dict_char_to_int}
+    for j in [0, 1, 2, 3, 4, 5]:
+        if text[j] in mapping[j].keys():
+            license_plate_ += mapping[j][text[j]]
+        else:
+            license_plate_ += text[j]
+
+    return license_plate_
+
+def read_license_plate(license_plate_crop):
+    
+    detections = reader.readtext(license_plate_crop)
+    
+    
+    for detection in detections:
+        bbox, text, score = detection
+
+        text = text.upper().replace(' ', '')
+        
+
+        if license_complies_format(text):
+            return format_license(text), score
+    return None, None
+
+def send_license_plate(license_plate_number):
+  url = 'http://localhost:6970/plate'
+  
+  data = {'plate': license_plate_number}
+  headers = {'Content-Type': 'application/json'}  
+  response = requests.post(url, data=json.dumps(data), headers=headers)
+  if response.status_code == 200:
+    return "La solicitud fue exitosa."
+  else:
+    return "Hubo un error en la solicitud:"
+
+
+    
+    
+    
+
+
